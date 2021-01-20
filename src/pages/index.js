bc08dafcf28f28ca97c2089fd625b4727c9aebfc
John Carlo Cunanan <codingjohn@outlook.com>
2021-01-20 08:17:00 +0800

    initialize gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const index = () => {
+  return (
+    <div>
+      <h1>Touche</h1>
+    </div>
+  )
 }
+
+export default index
